52d06206b18dd1d636d4b02890504287d86f2699
MAHDI ABDULSAHIB ALLAWI <mahdi.sahib@live.com>
2019-07-04 03:54:24 +0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: ['prettier', 'vue'],
+  // add your custom rules here
+  rules: {
+    semi: [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'prettier/prettier': ['error', { semi: false }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any'
+        }
+      }
+    ]
+  }
+}
